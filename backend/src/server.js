b47683bc43c3db6bfb5d7c641ae980a4e8a45405
23-faisal <a.faisal23r@gmail.com>
2024-12-29 14:07:06 +0600

    connect cloudinary

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import app from "./app.js";
 import "dotenv/config";
 import connectDB from "./config/connectDB.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 const port = process.env.PORT;
+
+connectCloudinary();
 
 app.listen(port, async () => {
   console.log("Server is running on port " + port);
