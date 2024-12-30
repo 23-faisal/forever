69ad83fd9038a35d19d95da188dfd7778ac03fd0
23-faisal <a.faisal23r@gmail.com>
2024-12-30 08:28:12 +0600

    create login functionality in the backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import userModel from "../models/user.model.js";
+import userLoginValidation from "../validation/userLoginValidation.js";
 import userRegistrationSchema from "../validation/userValidation.js";
 import bcrypt from "bcryptjs";
-
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 // User registration
 const registerUser = async (req, res, next) => {
@@ -58,10 +60,48 @@ const registerUser = async (req, res, next) => {
 // user login
 const loginUser = async (req, res, next) => {
   try {
-    res.status(200).json({
-      success: true,
-      message: "login Api is working",
+    const validateData = userLoginValidation.parse(req.body);
+
+    const { email, password } = validateData;
+
+    const userExists = await userModel.findOne({ email });
+
+    if (!userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exists!",
+      });
+    }
+
+    const passwordMatched = await bcrypt.compare(password, userExists.password);
+
+    if (!passwordMatched) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid  password",
+      });
+    }
+
+    const token = jwt.sign({ id: userExists._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
     });
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development", // Only send cookie over HTTPS in production
+        sameSite: "strict",
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: `${userExists.name} signed in successfully!`,
+        user: {
+          email: userExists.email,
+          username: userExists.name,
+        },
+        token,
+      });
   } catch (error) {
     next(error);
   }
